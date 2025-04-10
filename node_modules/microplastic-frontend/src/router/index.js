620be7b64a92cd8f769184758274815965f4ce2b
zhj21 <zhj21@gmail.com>
2025-04-10 23:45:14 +0800

    pages

--- node_modules/microplastic-frontend/src/router/index.js
+++ node_modules/microplastic-frontend/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import WaterSource from '../views/WaterSource.vue'
 import Food from '../views/Food.vue'
 import Air from '../views/Air.vue'
@@ -56,7 +56,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory('/Microplastic-Health-Evaluation-System/'),
   routes
 })
 
